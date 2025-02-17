229d513a4dce42229ad6db0c264ddceb4b8ff9f3
Aniruddha Tambe <aniruddhatambe@Aniruddhas-MacBook-Pro.local>
2025-02-17 14:31:47 -0500

    homepage created

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: HomepageComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full'}
+];
